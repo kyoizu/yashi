d3ec766419657215d1bbb4417e1f09b51bacb6e1
yashinano <yashinano@users.noreply.github.com>
2020-10-26 19:41:37 +0700

    First

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ client.on('message', message =>{
             return message.channel.send(`Ini profile picnya goshujin-sama >///< <${message.author.displayAvatarURL({format: `png`, dynamic: true})}>`);
         }
         else if(!message.mentions.users.size){
-            return message.channel.send(`Profile picture mu ini <${message.author.displayAvatarURL({format: 'png', dynamic: true})}>`);
+            return message.channel.send(`Profile picture mu ini <${message.author.displayAvatarURL({format: `png`, dynamic: true})}>`);
         }
         return message.channel.send(`Profile picturenya ${taggedUser.username} ini <${message.taggedUser.displayAvatarURL({format: `png`, dynamic: true})}>`);
     }
