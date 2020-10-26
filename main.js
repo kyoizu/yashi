9b75f95f4c2bc9b2abf438ae149a2bf904220cda
yashinano <yashinano@users.noreply.github.com>
2020-10-26 19:49:22 +0700

    First

--- main.js
+++ main.js
@@ -62,7 +62,10 @@ client.on('message', message =>{
         const taggedUser = message.mentions.users.first();
 
         if(message.member.roles.cache.has('769836747533713438')){
-            return message.channel.send(`Ini profile picnya goshujin-sama >///< <${message.author.displayAvatarURL({format: `png`, dynamic: true})}>`);
+            if(!message.mentions.users.size){
+                return message.channel.send(`Ini profile picnya goshujin-sama >///< <${message.author.displayAvatarURL({format: `png`, dynamic: true})}>`);
+            }
+            return message.channel.send(`Profile picturenya ${taggedUser.username} ini <${message.taggedUser.displayAvatarURL({format: `png`, dynamic: true})}>`);
         }
         else if(!message.mentions.users.size){
             return message.channel.send(`Profile picture mu ini <${message.author.displayAvatarURL({format: `png`, dynamic: true})}>`);
