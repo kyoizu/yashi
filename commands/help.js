9c35f1e321ecbd004d7c70c4358fe0cb709c120c
yashinano <yashinano@users.noreply.github.com>
2020-10-26 20:31:45 +0700

    Fis

--- commands/help.js
+++ commands/help.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'help',
     description: "list command",
     execute(message, args){
-        message.channel.send("/tpb (Link absen TPB)");
-        message.channel.send("/pacis (Link login PACIS)");
-        message.channel.send("/baca + 'kode 6 digit' || 'genre' (Tentu saja membaca manga)");
-        message.channel.send("/halo & /yashi (Sapa yashi ^-^)");
-        message.channel.send("/profpic + 'tag user' (kasih liat profpic)");
-        message.channel.send("/help (List command)");
+        message.channel.send
+        ("/tpb (Link absen TPB)\n /pacis (Link login PACIS)");
+        ("/baca + 'kode 6 digit' || 'genre' (Tentu saja membaca manga)");
+        ("/halo & /yashi (Sapa yashi ^-^)");
+        ("/profpic + 'tag user' (kasih liat profpic)");
+        ("/help (List command)");
     }
 }
