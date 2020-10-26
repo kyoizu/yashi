16c3611e3cb7561a5832402118cf234f3ce9aaee
yashinano <yashinano@users.noreply.github.com>
2020-10-26 19:38:02 +0700

    First

--- main.js
+++ main.js
@@ -65,9 +65,9 @@ client.on('message', message =>{
             return message.channel.send(`Profile picture mu ini <${message.author.displayAvatarURL({format: 'png', dynamic: true})}>`);
         }
         else if(message.member.roles.cache.has('769836747533713438')){
-            return message.channel.send(`Ini profile picnya goshujin-sama >///< <${message.author.displayAvatarURL({format: 'png', dynamic: true})}>`);
+            return message.channel.send(`Ini profile picnya goshujin-sama >///< <${message.author.displayAvatarURL({format: `png`, dynamic: true})}>`);
         }
-        return message.channel.send(`Profile picturenya ${taggedUser.username} ini <${message.taggedUser.displayAvatarURL({format: 'png', dynamic: true})}>`);
+        return message.channel.send(`Profile picturenya ${taggedUser.username} ini <${message.taggedUser.displayAvatarURL({format: `png`, dynamic: true})}>`);
     }
     else if(command === 'help'){
         client.commands.get('help').execute(message, args);
