2bf630eed72466c53d28d2f1aaf86f5977a6fe3b
yashinano <yashinano@users.noreply.github.com>
2020-11-13 17:40:05 +0700

    ...

--- main.js
+++ main.js
@@ -84,12 +84,41 @@ client.on('message', message =>{
     }
     else if(command === `show`)
     {
-        client.commands.get('show').execute(message, args);
+        var options = {
+            url: "http://results.dogpile.com/serp?qc=images&q=" + args,
+            method: "GET",
+            headers: {
+                "Accept": "text/html",
+                "User-Agent": "Chrome"
+            }
+        };
+        request(options, function(error, response, responseBody) {
+            if (error) {
+                return;
+            }
+     
+     
+            $ = cheerio.load(responseBody);
+     
+     
+            var links = $(".image a.link");
+     
+            var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+           
+            console.log(urls);
+     
+            if (!urls.length) {
+               
+                return;
+            }
+     
+            message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+        });
     }
 });
 
 
 
-client.login(process.env.token);
+client.login(process.env.token); //
 
 // afsbc04764
