8ad22bcf49b83c39ed83e3c887f60f05608d796a
yashinano <yashinano@users.noreply.github.com>
2020-11-23 15:48:04 +0700

    Update profpic.js

--- commands/profpic.js
+++ commands/profpic.js
@@ -3,11 +3,9 @@ module.exports = {
     description: "show PP",
     execute(message, args){
         const taggedUser = message.mentions.users.first();
-
+        const ppmu = message.author.displayAvatarURL({format: 'png', dynamic: true});
         if(!message.mentions.users.size){
-            message.channel.send(`Profile picture-mu ini <${message.taggedUser.displayAvatarURL({format: 'png', dynamic: true})}>`);
-            message.channel.send(message.author, attachment);
+            message.channel.send(`Profile picture-mu ini`, {files: [ppmu]});
         }
-        return message.channel.send(`Profile picturenya ${taggedUser.username} ini <${message.taggedUser.displayAvatarURL({format: 'png', dynamic: true})}>`);
     }
 }
